debe6236a43a772cda61b14d2deb6c6e22e95394
nightowl2002 <150873924+nightowl2002@users.noreply.github.com>
2024-04-27 19:08:04 -0400

    Update helloworld.py

--- DSA456-DataStructures-and-Algorithms-main/DSA - labs/lab0/helloworld.py
+++ DSA456-DataStructures-and-Algorithms-main/DSA - labs/lab0/helloworld.py
@@ -1,9 +1,9 @@
 """
-Author      : Soni Dev.
-UserName    : DASONI4
-StudentNo   : 130759210.
-StudentEmail: dasoni4@myseneca.ca
-Date        : 17th Sept 2023.
+Author      : Jatin.
+UserName    : jatin4
+StudentNo   : 141358218.
+StudentEmail: jatin4@myseneca.ca
+Date        : 17th Jan 2024.
 Description : Hello World - first python print command program.
 """
 # This will print Hello World.
